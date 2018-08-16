60fecd24e26e37905b49341524b674d3c39dbcce
kranzkrone <dev@st-page.de>
2018-08-16 20:09:29 +0200

    Start projectdevelopment - MediaRight Management Tool

---
+++ wpjwst-picturerights.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: MediaRight Management Tool
+Plugin URI:   https://github.com/KranzKrone/wpjwst-picturerights
+Description:  Media right management tool for Pictures 
+Version: 0.0.0
+Author: kranzkrone
+Author URI: https://github.com/KranzKrone/
+License: GNU General Public License 3      
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  mrmt
+Domain Path:  /languages
+*/
+
+/**
+Alle Funktionen welche (de)-installiert werden müssen, sind hier zu impementieren. 
+*/
+function install(){
+    return null;
+}
+
+function uninstall(){
+    return null;
+}
